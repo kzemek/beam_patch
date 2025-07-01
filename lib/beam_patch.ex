0b93b703c5fd522abdb2bf11684503b220e26d5f
Konrad Zemek <konrad.zemek@gmail.com>
2025-07-02 01:42:27 +0200

    `mix new beam_patch`

---
+++ lib/beam_patch.ex
@@ -0,0 +1,18 @@
+defmodule BeamPatch do
+  @moduledoc """
+  Documentation for `BeamPatch`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BeamPatch.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/beam_patch_test.exs
@@ -0,0 +1,8 @@
+defmodule BeamPatchTest do
+  use ExUnit.Case
+  doctest BeamPatch
+
+  test "greets the world" do
+    assert BeamPatch.hello() == :world
+  end
+end
